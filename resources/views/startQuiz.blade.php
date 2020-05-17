09fad7ab5ed28b0bd6e49dac9a3fb22d59cbb60e
OkoyePeter <Okoyep98@gmail.com>
2020-05-17 22:12:18 +0100

    question display is fixed

--- resources/views/startQuiz.blade.php
+++ resources/views/startQuiz.blade.php
@@ -28,7 +28,7 @@
 		</div>
 		<div class="col-9">
 			<form action="{{ route('mark') }}" method="post">
-            {{-- subjects begins --}}
+                {{-- subjects begins --}}
 				<ul class="nav nav-pills nav-justify mb-4" role="tablist">
                     @foreach($courses as $key =>$course)
                         <li  class="nav-item">
@@ -36,308 +36,58 @@
                  		</li>
                     @endforeach
 				</ul>
-            {{-- subjects ends --}}
+                {{-- subjects ends --}}
+                @php
+                  $i = 1;
+                @endphp
+                {{-- question begins --}}
+
+                @foreach($questions as $subject => $question)
                     @php
-                        $i = 1;
+                        $z = array_search($subject, $courses) + 1;
                     @endphp
-				<div class="tab-content">
-                    @foreach($questions as $subject => $question)
-                        <div class="tab-pane  fade @if($courses[0] === $subject) active show @endif" id="tab{{$i++}}">
-                            <div class="tab-content" style="margin-bottom:50px">
-                                @foreach($question as $index => $content)
-                                    <div class="tab-pane fade @if($index === 0) active show @endif" role="tabpanel" id="tab1-{{ ++$index }}">
-                                        {{-- quetions --}}
-                                        <p class="d-flex">
-                                            <span class="mr-4">{{ $index }}.</span>
-                                            <span>{{ $content->question }}</span>
-                                        </p>
-                                    </div>
-                                @endforeach
-                            </div>
-
+                    @if($courses[0] === $subject)<div class="tab-content"> @endif
+                    <div id="tab{{$i++}}" class="tab-pane  fade @if($courses[0] === $subject) active show @endif">
+                        <div class="tab-content" style="margin-bottom:50px">
+                            @foreach($question as $index => $content)
+                                <div class="tab-pane fade @if($index === 0) active show @endif" role="tabpanel" id="tab{{$i}}-{{ ++$index }}">
+                                    {{-- quetions --}}
+                                    <p class="d-flex">
+                                        <span class="mr-4">{{ $index }}.</span>
+                                        <span>{{ $content->question }}</span>
+                                    </p>
+                                    {{-- shuffle question --}}
+                                    @php
+                                      $options = array($content->answer, $content->option1, $content->option2, $content->option3);
+                                      shuffle($options);
+                                    @endphp
+                                    {{-- display option --}}
+                                    <p class="d-flex flex-column">
+                                        @foreach($options as $x => $option)
+                                            <span>
+											    <input type="radio" name="{{ $subject."_".$index }}[]" value="{{ $option }}"> {{ $option }}
+                                            </span>
+                                        @endforeach
+                                    </p>
+                                </div>
+                            @endforeach
                         </div>
-                    @endforeach
-{{--					<div class="tab-pane fade active show" id="tab1">--}}
-
-{{--						 first subject questions --}}
-{{--						<div class="tab-content" style="margin-bottom:50px">--}}
-{{--							@for ($i = 1; $i <= count($coursesQuestion[$keys[0]]); $i++)--}}
-{{--								@php--}}
-{{--									$answer = $coursesQuestion[$keys[0]][($i - 1)]->answer;--}}
-{{--									$option1 = $coursesQuestion[$keys[0]][($i - 1)]->option1;--}}
-{{--									$option2 = $coursesQuestion[$keys[0]][($i - 1)]->option2;--}}
-{{--									$option3 = $coursesQuestion[$keys[0]][($i - 1)]->option3;--}}
-
-{{--									$option = array($answer, $option1, $option2, $option3);--}}
-{{--									shuffle($option);--}}
-{{--								@endphp--}}
-{{--								@if ($i == 1)--}}
-{{--									<div class="tab-pane fade active show" role="tabpanel" id="tab1-{{ $i }}">--}}
-{{--										<p class="d-flex">--}}
-{{--											<span class="mr-4">{{ $i }}.</span>--}}
-{{--											<span>{{ $coursesQuestion[$keys[0]][($i - 1)]->question }}</span>--}}
-{{--										</p>--}}
-{{--										<p class="d-flex flex-column">--}}
-{{--											@for ($x = 0; $x < count($option); $x++)--}}
-{{--												<span>--}}
-{{--													<input type="radio" name="{{ $keys[0].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--												</span>--}}
-{{--											@endfor--}}
-{{--										</p>--}}
-{{--									</div>--}}
-{{--								@else--}}
-
-{{--								<div class="tab-pane fade" role="tabpanel" id="tab1-{{ $i }}">--}}
-{{--									<p class="d-flex">--}}
-{{--										<span class="mr-4">{{ $i }}.</span>--}}
-
-{{--										<span>{{ $coursesQuestion[$keys[0]][($i - 1)]->question }}</span>--}}
-{{--									</p>--}}
-{{--									<p class="d-flex flex-column">--}}
-{{--										@for ($x = 0; $x < count($option); $x++)--}}
-{{--											<span>--}}
-{{--												<input type="radio" name="{{ $keys[0].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--											</span>--}}
-{{--										@endfor--}}
-{{--									</p>--}}
-{{--								</div>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-{{--					</div>--}}
-{{--					 frirst subject questions ends --}}
-
-{{--					 first subject question number --}}
-{{--					<ul class="nav nav-pills pagination">--}}
-{{--						@for ($i = 1; $i <= count($coursesQuestion[$keys[0]]); $i++)--}}
-{{--							@if ($i == 1)--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link active page-link" a href="#tab1-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@else--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link page-link" a href="#tab1-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-{{--					</ul>--}}
-{{--					 first subject question ends --}}
-{{--				</div>--}}
-{{--				 first subject ends --}}
-
-
-{{--				 second subject --}}
-{{--				<div class="tab-pane fade" id="tab2">--}}
-
-{{--					 second subject question --}}
-{{--					<div class="tab-content" style="margin-bottom:50px">--}}
-{{--						@for ($i = 1; $i <= count($coursesQuestion[$keys[1]]); $i++)--}}
-{{--							@php--}}
-{{--								$answer = $coursesQuestion[$keys[1]][($i - 1)]->answer;--}}
-{{--								$option1 = $coursesQuestion[$keys[1]][($i - 1)]->option1;--}}
-{{--								$option2 = $coursesQuestion[$keys[1]][($i - 1)]->option2;--}}
-{{--								$option3 = $coursesQuestion[$keys[1]][($i - 1)]->option3;--}}
-
-{{--								$option = array($answer, $option1, $option2, $option3);--}}
-{{--								shuffle($option);--}}
-{{--							@endphp--}}
-{{--								@if ($i == 1)--}}
-{{--									<div class="tab-pane fade active show" role="tabpanel" id="tab2-{{ $i }}">--}}
-{{--										<p class="d-flex">--}}
-{{--											<span class="mr-4">{{ $i }}.</span>--}}
-{{--											<span>{{ $coursesQuestion[$keys[1]][($i - 1)]->question }}</span>--}}
-{{--										</p>--}}
-{{--										<p class="d-flex flex-column">--}}
-{{--											@for ($x = 0; $x < count($option); $x++)--}}
-{{--												<span>--}}
-{{--													<input type="radio" name="{{ $keys[1].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--												</span>--}}
-{{--											@endfor--}}
-{{--										</p>--}}
-{{--									</div>--}}
-{{--								@else--}}
-
-{{--								<div class="tab-pane fade" role="tabpanel" id="tab2-{{ $i }}">--}}
-{{--									<p class="d-flex">--}}
-{{--										<span class="mr-4">{{ $i }}.</span>--}}
-
-{{--										<span>{{ $coursesQuestion[$keys[1]][($i - 1)]->question }}</span>--}}
-{{--									</p>--}}
-{{--									<p class="d-flex flex-column">--}}
-{{--										@for ($x = 0; $x < count($option); $x++)--}}
-{{--											<span>--}}
-{{--												<input type="radio" name="{{ $keys[1].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--											</span>--}}
-{{--										@endfor--}}
-{{--									</p>--}}
-{{--								</div>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-
-{{--					</div>--}}
-{{--					 second subject question ends --}}
-
-{{--					 second subject question number --}}
-{{--					<ul class="nav nav-pills pagination">--}}
-{{--						@for ($i = 1; $i <= count($coursesQuestion[$keys[1]]); $i++)--}}
-{{--							@if ($i == 1)--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link active page-link" a href="#tab2-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@else--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link page-link" a href="#tab2-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-{{--					</ul>--}}
-{{--					 second subject question number --}}
-{{--				</div>--}}
-{{--				 second subject ends --}}
-
-
-{{--				 third subject --}}
-{{--				<div class="tab-pane fade" id="tab3">--}}
-
-{{--					 third subject question --}}
-{{--					<div class="tab-content" style="margin-bottom:50px">--}}
-{{--						@for ($i = 1; $i <= count($coursesQuestion[$keys[2]]); $i++)--}}
-{{--							@php--}}
-{{--								$answer = $coursesQuestion[$keys[2]][($i - 1)]->answer;--}}
-{{--								$option1 = $coursesQuestion[$keys[2]][($i - 1)]->option1;--}}
-{{--								$option2 = $coursesQuestion[$keys[2]][($i - 1)]->option2;--}}
-{{--								$option3 = $coursesQuestion[$keys[2]][($i - 1)]->option3;--}}
-
-{{--								$option = array($answer, $option1, $option2, $option3);--}}
-{{--								shuffle($option);--}}
-{{--							@endphp--}}
-{{--								@if ($i == 1)--}}
-{{--									<div class="tab-pane fade active show" role="tabpanel" id="tab3-{{ $i }}">--}}
-{{--										<p class="d-flex">--}}
-{{--											<span class="mr-4">{{ $i }}.</span>--}}
-{{--											<span>{{ $coursesQuestion[$keys[2]][($i - 1)]->question }}</span>--}}
-{{--										</p>--}}
-{{--										<p class="d-flex flex-column">--}}
-{{--											@for ($x = 0; $x < count($option); $x++)--}}
-{{--												<span>--}}
-{{--													<input type="radio" name="{{ $keys[2].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--												</span>--}}
-{{--											@endfor--}}
-{{--										</p>--}}
-{{--									</div>--}}
-{{--								@else--}}
-
-{{--								<div class="tab-pane fade" role="tabpanel" id="tab3-{{ $i }}">--}}
-{{--									<p class="d-flex">--}}
-{{--										<span class="mr-4">{{ $i }}.</span>--}}
-
-{{--										<span>{{ $coursesQuestion[$keys[2]][($i - 1)]->question }}</span>--}}
-{{--									</p>--}}
-{{--									<p class="d-flex flex-column">--}}
-{{--										@for ($x = 0; $x < count($option); $x++)--}}
-{{--											<span>--}}
-{{--												<input type="radio" name="{{ $keys[2].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--											</span>--}}
-{{--										@endfor--}}
-{{--									</p>--}}
-{{--								</div>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-
-{{--					</div>--}}
-{{--					 third subject question ends --}}
-
-{{--					 third subject question number --}}
-{{--					<ul class="nav nav-pills pagination">--}}
-{{--						@for ($i = 1; $i <= count($coursesQuestion[$keys[2]]); $i++)--}}
-{{--							@if ($i == 1)--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link active page-link" a href="#tab3-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@else--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link page-link" a href="#tab3-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-{{--					</ul>--}}
-{{--					 third subject question number --}}
-{{--				</div>--}}
-{{--				 third subject ends --}}
-
-{{--				 fourth subject --}}
-{{--				<div class="tab-pane fade" id="tab4">--}}
-
-{{--					 fourth subject question --}}
-{{--					<div class="tab-content" style="margin-bottom:50px">--}}
-{{--						@for ($i = 1; $i <= count($coursesQuestion[$keys[3]]); $i++)--}}
-{{--							@php--}}
-{{--								$answer = $coursesQuestion[$keys[3]][($i - 1)]->answer;--}}
-{{--								$option1 = $coursesQuestion[$keys[3]][($i - 1)]->option1;--}}
-{{--								$option2 = $coursesQuestion[$keys[3]][($i - 1)]->option2;--}}
-{{--								$option3 = $coursesQuestion[$keys[3]][($i - 1)]->option3;--}}
-
-{{--								$option = array($answer, $option1, $option2, $option3);--}}
-{{--								shuffle($option);--}}
-{{--							@endphp--}}
-{{--								@if ($i == 1)--}}
-{{--									<div class="tab-pane fade active show" role="tabpanel" id="tab4-{{ $i }}">--}}
-{{--										<p class="d-flex">--}}
-{{--											<span class="mr-4">{{ $i }}.</span>--}}
-{{--											<span>{{ $coursesQuestion[$keys[3]][($i - 1)]->question }}</span>--}}
-{{--										</p>--}}
-{{--										<p class="d-flex flex-column">--}}
-{{--											@for ($x = 0; $x < count($option); $x++)--}}
-{{--												<span>--}}
-{{--													<input type="radio" name="{{ $keys[3].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--												</span>--}}
-{{--											@endfor--}}
-{{--										</p>--}}
-{{--									</div>--}}
-{{--								@else--}}
-
-{{--								<div class="tab-pane fade" role="tabpanel" id="tab4-{{ $i }}">--}}
-{{--									<p class="d-flex">--}}
-{{--										<span class="mr-4">{{ $i }}.</span>--}}
-
-{{--										<span>{{ $coursesQuestion[$keys[3]][($i - 1)]->question }}</span>--}}
-{{--									</p>--}}
-{{--									<p class="d-flex flex-column">--}}
-{{--										@for ($x = 0; $x < count($option); $x++)--}}
-{{--											<span>--}}
-{{--												<input type="radio" name="{{ $keys[3].$i }}[]" value="{{ $option[$x] }}"> {{ $option[$x] }}--}}
-{{--											</span>--}}
-{{--										@endfor--}}
-{{--									</p>--}}
-{{--								</div>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-
-{{--					</div>--}}
-{{--					 fourth subject question ends --}}
-
-{{--					 fourth subject question number --}}
-{{--					<ul class="nav nav-pills pagination">--}}
-{{--						@for ($i = 1; $i <= count($coursesQuestion[$keys[3]]); $i++)--}}
-{{--							@if ($i == 1)--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link active page-link" a href="#tab4-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@else--}}
-{{--								<li  class="nav-item page-item">--}}
-{{--									<a class="nav-link page-link" a href="#tab4-{{$i}}" data-toggle="tab">{{ $i }}</a>--}}
-{{--								</li>--}}
-{{--							@endif--}}
-{{--						@endfor--}}
-{{--					</ul>--}}
-{{--					 fourth subject question number --}}
-{{--				</div>--}}
-{{--				--}}{{-- fourth subjects ends --}}
-{{--				@csrf--}}
-{{--				<input type="submit" value="Submit" class="btn btn-success mt-5">--}}
+                        {{-- subject question number --}}
+                        <ul class="nav nav-pills pagination">
+                            @for ($x = 1; $x <= count($question); $x++)
+                                <li  class="nav-item page-item">
+                                    <a class="nav-link @if($x == 1) active @endif page-link" href="#tab{{$i}}-{{$x}}" data-toggle="tab">{{ $x }}</a>
+                                </li>
+                            @endfor
+                        </ul>
+                        {{-- subject question ends --}}
                     </div>
-			</form>
-		</div>
-	</div>
- </div>
+                @endforeach
+                @csrf
+                <input type="submit" value="Submit" class="btn btn-success mt-5">
+            </form>
+        </div>
+    </div>
+</div>
 
 @endsection
